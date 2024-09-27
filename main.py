70e2d5015b92f76cf9a12a2975511e92f240d83f
Arv1ndRana <arvindrana6969@gmail.com>
2024-09-27 17:40:31 +0545

    added movement

--- main.py
+++ main.py
@@ -36,8 +36,25 @@ class Food:
         canvas.create_oval(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill= FOOD_COLOR, tag= "food")
 
 
-def next_turn():
-    pass
+def next_turn(snake, food):
+    x, y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0, (x,y))
+    square = canvas.create_rectangle(x,y,x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+    snake.squares.insert(0,square)
+
+    window.after(SPEED,next_turn,snake,food)
+
 
 def change_direction(new_direction):
     pass
@@ -77,6 +94,7 @@ window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 snake = Snake()
 food = Food()
+next_turn(snake,food)
 
 
 
