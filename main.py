16a58b2e0fd4d565853e0fb42fa54773025ce7b1
Arv1ndRana <arvindrana6969@gmail.com>
2024-09-27 18:03:22 +0545

    added collision with border and self

--- main.py
+++ main.py
@@ -67,8 +67,10 @@ def next_turn(snake, food):
         canvas.delete(snake.squares[-1])
 
         del snake.squares[-1]
-
-    window.after(SPEED,next_turn,snake,food)
+    if check_collision(snake):
+        game_over()
+    else:
+        window.after(SPEED,next_turn,snake,food)
 
 
 def change_direction(new_direction):
@@ -90,8 +92,20 @@ def change_direction(new_direction):
         if direction != "up":
             direction = new_direction
 
-def check_collision():
-    pass
+def check_collision(snake):
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
 
 def game_over():
     pass
