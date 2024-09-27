a1bd8753222e980a1e163528d4620d8f158406fa
Arv1ndRana <arvindrana6969@gmail.com>
2024-09-27 17:33:02 +0545

    added green box

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ FOOD_COLOR = "RED"
 BACKGROUND_COLOR = "BLACK"
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0,BODY_PARTS):
+            self.coordinates.append([0,0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+
 
 class Food:
 
@@ -23,6 +34,7 @@ class Food:
         self.coordinates = [x,y]
 
         canvas.create_oval(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill= FOOD_COLOR, tag= "food")
+
 
 def next_turn():
     pass
