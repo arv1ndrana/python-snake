7d04434480f9e0466273e6e59c2b8766bc1443e5
Arv1ndRana <arvindrana6969@gmail.com>
2024-09-27 17:00:06 +0545

    init and boilerplate

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "GREEN"
+FOOD_COLOR = "RED"
+BACKGROUND_COLOR = "BLACK"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+
+window.title("Snake Game")
+window.resizable(False,False) 
+
+window.mainloop()
