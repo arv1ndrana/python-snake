cdecf1505a6465030cead550ff911bdb7b426d0e
Arv1ndRana <arvindrana6969@gmail.com>
2024-09-27 17:11:09 +0545

    added centering the window

--- main.py
+++ main.py
@@ -41,4 +41,20 @@ label.pack()
 canvas = Canvas(window, bg = BACKGROUND_COLOR, height = GAME_HEIGHT, width = GAME_WIDTH)
 canvas.pack()
 
+window.update()
+
+# For centering the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+# ---------------------------------------------------------- #
+
+
+
 window.mainloop()
